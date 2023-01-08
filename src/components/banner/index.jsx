e043c37354c61857098d10f9e965ff7c63c653f9
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-07 20:07:29 -0500

    feat: Criei a base do banner e a estilização

---
+++ src/components/banner/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from './styled'
+
+export default function Banner() {
+  return (
+    <Container>
+        <div id='banner'></div>
+    </Container>
+  )
+}
---
+++ src/components/banner/styled.js
@@ -0,0 +1,11 @@
+import styled, { StyledComponent } from "styled-components";
+
+const Container = styled.banner`
+    div#banner {
+        background-color: #505050;
+        height: 10rem;
+        width: 100%;
+    }
+`;
+
+export default Container;
