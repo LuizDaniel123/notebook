87668aef74e3bdd2328f0b316300c1ba01eb9a4c
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-07 20:06:43 -0500

    feat: Adicionei a importação do banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Banner from './components/banner';
 import Header from './components/header';
 
 function App() {
   return (
     <>
     <Header />
+    <Banner />
     </>
   );
 }
